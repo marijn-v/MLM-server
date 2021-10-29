eb1ce4e45b968b78cf6ea23b7bec56beaaa85195
marijn-v <marijnverbiesen@gmail.com>
2021-10-29 15:05:18 +0200

    restore missing migrations

---
+++ migrations/20211020122243-create-responses.js
@@ -0,0 +1,30 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("responses", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      expression: {
+        type: Sequelize.STRING,
+      },
+      translation: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("responses");
+  },
+};
